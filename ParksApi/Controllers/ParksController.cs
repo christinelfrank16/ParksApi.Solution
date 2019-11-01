11b3f8ce80907d126d08ac28989cb5440276fe09
Christine Frank <christine.braun13@gmail.com>
2019-11-01 16:00:50 -0700

    Add search options for parks and park animals

--- ParksApi/Controllers/ParksController.cs
+++ ParksApi/Controllers/ParksController.cs
@@ -26,11 +26,59 @@ namespace ParksApi.Controllers
             return _db.Parks.ToList();
         }
 
+        // GET api/parks/search
+        [HttpGet("search")]
+        public ActionResult<IEnumerable<Park>> SearchParks(string name, string distinction, string state)
+        {
+            var query = _db.Parks.Include(park => park.VisitorCenterAddresses).AsQueryable();
+            if(name != null)
+            {
+                query = query.Where(entry => entry.Name.ToLower().Contains(name.ToLower()));
+            }
+            if (distinction != null)
+            {
+                query = query.Where(entry => entry.Designation.ToLower().Contains(distinction.ToLower()));
+            }
+            if(state != null)
+            {
+                query = query.Where(entry => entry.VisitorCenterAddresses.Any(address => address.State == state));
+            }
+            return query.ToList();
+        }
+
         // GET api/parks/5
         [HttpGet("{parkId:int}")]
         public ActionResult<Park> Get(int parkId)
         {
             return _db.Parks.Include(park => park.Fees).Include(park => park.VisitorCenterAddresses).Include(park => park.Animals).ThenInclude(wildlife => wildlife.Animal).FirstOrDefault(park => park.ParkId == parkId);
+        }
+
+        // GET api/parks/5/animals/search
+        [HttpGet("{parkId:int}/animals/search")]
+        public ActionResult<IEnumerable<Animal>> SearchParkAnimals([FromRoute] int parkId, string commonname, string type, string diet)
+        {
+            var query = _db.LocalWildlife.Include(wildlife => wildlife.Animal).Where(wildlife => wildlife.ParkId == parkId).Select(wildlife => wildlife.Animal).AsQueryable();
+            if (commonname != null)
+            {
+                if(commonname.Contains(" "))
+                {
+                    List<string> searchterms = commonname.ToLower().Split(" ").ToList();
+                    query = query.Where(animal => animal.CommonName.ToLower().Split(" ", default).ToList().Any(namevalue => searchterms.Contains(namevalue)));
+                }
+                else
+                {
+                    query = query.Where(animal => animal.CommonName.ToLower().Contains(commonname.ToLower()));
+                }
+            }
+            if (type != null)
+            {
+                query = query.Where(animal => animal.Type.ToLower().Contains(type.ToLower()));
+            }
+            if (diet != null)
+            {
+                query = query.Where(animal => animal.Diet.ToLower().Contains(diet.ToLower()));
+            }
+            return query.ToList();
         }
 
         //GET api/parks/5/animals
