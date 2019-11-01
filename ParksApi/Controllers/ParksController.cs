97db57d9ab985b69d7b23ac51237041c74680ea8
Christine Frank <christine.braun13@gmail.com>
2019-11-01 10:31:24 -0700

    Add routes for park-specific animal entries

--- ParksApi/Controllers/ParksController.cs
+++ ParksApi/Controllers/ParksController.cs
@@ -27,14 +27,14 @@ namespace ParksApi.Controllers
         }
 
         // GET api/parks/5
-        [HttpGet("{id}")]
+        [HttpGet("{id:int}")]
         public ActionResult<Park> Get(int id)
         {
             return _db.Parks.Include(park => park.Fees).Include(park => park.VisitorCenterAddresses).Include(park => park.Animals).ThenInclude(wildlife => wildlife.Animal).FirstOrDefault(park => park.ParkId == id);
         }
 
         //GET api/parks/5/animals
-        [HttpGet("{id}/animals")]
+        [HttpGet("{id:int}/animals")]
         public ActionResult<IEnumerable<Animal>> GetWildlife(int id)
         {
             Park thisPark = _db.Parks.Include(park => park.Animals).ThenInclude(wildlife => wildlife.Animal).FirstOrDefault(park => park.ParkId == id);
@@ -51,7 +51,7 @@ namespace ParksApi.Controllers
 
 
         // PUT api/parks/5
-        [HttpPut("{id}")]
+        [HttpPut("{id:int}")]
         public void Put(int id, [FromBody] Park park)
         {
             park.ParkId = id;
@@ -60,11 +60,20 @@ namespace ParksApi.Controllers
         }
 
         // DELETE api/parks/5
-        [HttpDelete("{id}")]
+        [HttpDelete("{id:int}")]
         public void Delete(int id)
         {
             Park thisPark = _db.Parks.FirstOrDefault(park => park.ParkId == id);
             _db.Parks.Remove(thisPark);
+            _db.SaveChanges();
+        }
+
+        // DELETE api/parks/5/animals/2
+        [HttpDelete("{parkId:int}/animals/{animalId:int}")]
+        public void RemoveWildlifeReference([FromRoute] int parkId, [FromRoute] int animalId)
+        {
+            LocalWildlife joinEntry = _db.LocalWildlife.FirstOrDefault(entry => entry.ParkId == parkId && entry.AnimalId == animalId);
+            _db.LocalWildlife.Remove(joinEntry);
             _db.SaveChanges();
         }
     }
